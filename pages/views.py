9597fe31f2fd8b779614abc55fc29e82e687c3f6
reverse-m <whiteball134@gmail.com>
2020-12-13 10:27:49 +0600

    initial commit01

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
-    
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView): # new
+    template_name = 'about.html'
